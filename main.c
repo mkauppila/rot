b777d075ef5678d393a6eef145c0b32f5bec18e6
Markus Kauppila <markus.kauppila@gmail.com>
2018-04-09 21:49:32 +0300

    Add ROT function

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
+char rot(char ch, int rot, int reverse) {
+  if ('A' <= ch && ch <= 'Z') {
+    const int base = (int)'A';
+    const int alphaCount = ('Z' - 'A') + 1;
+    const int arot = reverse ? (alphaCount - rot) : rot;
+
+    return (((ch - base) + arot) % alphaCount) + base;
+  } else {
+    return ch;
+  }
+}
+
 int main(int argc, char *argp[]) {
-  printf("Do ROT13 for stdin");
   while (1) {
     char ch = fgetc(stdin);
-    if (feof(stdin)) break;
-    fputc(ch, stdout);
+    if (feof(stdin)) {
+      break;
+    }
+    fputc(rot(ch, 1, 1), stdout);
   }
 
   return 0;
