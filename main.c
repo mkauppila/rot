9214765b6e6c83bb8df81fea51b06ab55d7e4ca2
Markus Kauppila <markus.kauppila@gmail.com>
2018-04-10 19:49:04 +0300

    Handle commmand line args

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-
+#include <string.h>
 #include "rot.h"
 
+void printUsage() {
+  fprintf(stdout, "Usage:\n");
+  fprintf(stdout, "rot [-cd] < STDIN\n");
+}
+
 int main(int argc, char *argv[]) {
-  // TODO handle command line args
+  mode_t mode = CIPHER;
+
+  if (argc == 2) {
+    if (strncmp("-c", argv[1], 2) == 0) {
+      mode = CIPHER;
+    } else if (strncmp("-d", argv[1], 2) == 0) {
+      mode = DECIPHER;
+    } else {
+      printUsage();
+    }
+  } else {
+    printUsage();
+  }
 
   while (1) {
     char ch = fgetc(stdin);
     if (feof(stdin)) {
       break;
     }
-    fputc(rot(ch, 1, CIPHER), stdout);
+    fputc(rot(ch, 1, mode), stdout);
   }
 
   return 0;
