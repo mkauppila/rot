f573b93af7acbfed539c4f540156018d7bbf07c3
Markus Kauppila <markus.kauppila@gmail.com>
2018-04-11 08:22:52 +0300

    Support all ascii chars from space to ~

--- rot.c
+++ rot.c
@@ -1,14 +1,14 @@
 #include "rot.h"
 
 char rot(char ch, int rot, mode_t mode) {
-  int reverse = mode == DECIPHER;
+  const char from = ' ';
+  const char to = '~';
 
-  if ('A' <= ch && ch <= 'Z') {
-    const int base = (int)'A';
-    const int alphaCount = ('Z' - 'A') + 1;
-    const int shift = reverse ? (alphaCount - rot) : rot;
+  if (from <= ch && ch <= to) {
+    const int range = (to - from) + 1;
+    const int shift = mode == CIPHER ? rot : (range - rot);
 
-    return (((ch - base) + shift) % alphaCount) + base;
+    return (((ch - from) + shift) % range) + from;
   } else {
     return ch;
   }
